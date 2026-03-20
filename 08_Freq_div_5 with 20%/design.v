fc3f3757419f0e5eaac5511cec2555b94c38f913
suresh2327 <sureshgadi68@gmail.com>
2026-03-20 08:52:53 +0530

    Freq div by 5 with 20% duty cycle

---
+++ 08_Freq_div_5 with 20%/design.v
@@ -0,0 +1,25 @@
+// Code your design here
+module freq_div_5(
+  input clk,rst,
+  output f_5
+);
+  reg [2:0]count;
+  
+  always@(posedge clk)
+    begin
+      if(!rst)
+        begin
+          count<=3'b000;
+        end
+      else if(count==3'b100)
+        begin
+          count<=3'b000;
+        end
+      else
+        begin
+          count<=count+1'b1;
+        end
+    end
+  
+  assign f_5=count[2];
+endmodule
